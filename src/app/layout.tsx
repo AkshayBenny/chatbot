86b807ef77b095d2f2126a417b708acfdf04dbba
Akshay Benny <akshay@mx-zenbook>
2023-07-27 20:26:36 +0530

    basic setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import { ClerkProvider } from "@clerk/nextjs";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "AI Getting Started",
-  description: "Help you setup an AI project with ease",
+  title: "Doubtss",
+  description: "Help you pass UPSC exam with ease",
 };
 
 export default function RootLayout({
